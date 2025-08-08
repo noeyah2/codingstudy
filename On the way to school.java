ec21c79d0e8149a34007501b4572441f40e456b5
Hayeon Kang <108393035+noeyah2@users.noreply.github.com>
2025-08-08 09:29:44 +0900

    250808

---
+++ On the way to school.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+
+        for (int[] puddle : puddles) {
+            dp[puddle[1] - 1][puddle[0] - 1] = -1;
+        }
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 && j == 0) || dp[i][j] == -1) continue;
+
+                int top = (i == 0 || dp[i - 1][j] == -1) ? 0 : dp[i - 1][j];
+                int left = (j == 0 || dp[i][j - 1] == -1) ? 0 : dp[i][j - 1];
+
+                dp[i][j] = (top + left) % 1000000007;
+            }
+        }
+
+        return dp[n - 1][m - 1];
+    }
+}
