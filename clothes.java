787441c9cdc057a2a5c581579006703da6bc52e1
Hayeon Kang <108393035+noeyah2@users.noreply.github.com>
2025-08-26 10:11:16 +0900

    250826

---
+++ clothes.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+
+        int answer = 1;
+        
+        for (int cnt : map.values()) {
+            answer *= (cnt + 1);
+        }
+        
+        return answer - 1;
+    }
+}
