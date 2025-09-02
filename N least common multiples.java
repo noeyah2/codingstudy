b7c5538c1e577d2c94857e0823c854ef7839b56f
Hayeon Kang <108393035+noeyah2@users.noreply.github.com>
2025-09-02 09:52:39 +0900

    250902

---
+++ N least common multiples.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] arr) {
+        int ans = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            ans = lcm(ans, arr[i]);
+        }
+        return ans;
+    }
+    
+    public static int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+    
+    public static int lcm(int a, int b) {
+        return (a*b) / gcd(a,b);
+    }
+}
